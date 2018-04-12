957deb1cb898d4530c2dc981cf7554e5b5f5e65f
mata <gaoyp_1@sina.com>
2018-04-12 14:26:54 +0800

    Custom methods to store json data and use scrappy's own JsonItemExporter to store data

--- ArtSpider/pipelines.py
+++ ArtSpider/pipelines.py
@@ -4,8 +4,11 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import codecs
+import json
 
 from scrapy.pipelines.images import ImagesPipeline
+from scrapy.exporters import JsonItemExporter
 
 
 class ArtspiderPipeline(object):
@@ -24,3 +27,40 @@ class ArticleImagePipeline(ImagesPipeline):
         item['front_image_path'] = front_image_path
         # 返回item以方便后面的pipeline对item进行处理
         return item
+
+
+class JsonWithEncodingPipeline(object):
+    """
+    自定义JsonWithEncodingPipeline，用于存放json数据
+    """
+
+    def __init__(self):
+        self.file = codecs.open("article.json", "w", encoding='utf-8')
+
+    def process_item(self, item, spider):
+        # 设置ensure_ascii=False防止存入中文时出现乱码
+        lines = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(lines)
+        return item
+
+    def spider_closed(self, spider):
+        self.file.close()
+
+
+class JsonExporterPipleline(object):
+    """
+    利用scrapy定义的JsonItemExporter导出json数据
+    """
+
+    def __init__(self):
+        self.file = open('articleexport.json', "wb")  # wb以二进制的方式打开
+        self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+        self.exporter.start_exporting()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+    def close_spider(self):
+        self.exporter.finish_exporting()
+        self.file.close()
